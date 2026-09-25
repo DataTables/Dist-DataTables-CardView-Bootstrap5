49a905d9a25c53020c84055be0e7a40ae402a826
dtbuild <dtbuild@rigil>
2026-09-25 11:04:51 +0000

    5bc8d4ed681a29a34ae2ee9578eedc247fba00a3 Release 1.1.2
    Sync to source repo @5bc8d4ed681a29a34ae2ee9578eedc247fba00a3

--- js/cardView.bootstrap5.js
+++ js/cardView.bootstrap5.js
@@ -1,4 +1,4 @@
-/*! CardView Bootstrap 5 styling 1.1.1 for DataTables
+/*! CardView Bootstrap 5 styling 1.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 
